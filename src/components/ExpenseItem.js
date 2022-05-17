b38568a71f29de8d5dc378bab7b8b44f76e0779f
İhlas Mert <88083556+ihlasMert@users.noreply.github.com>
2022-05-17 11:20:54 +0300

    created todoApp by comps and props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+import "./ExpenseDate"
+
+function ExpenseItem(props) {
+ 
+
+  return <div className="expense-item">
+      <ExpenseItem date={props.date}></ExpenseItem>
+      <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+      </div>
+  </div>
+}
+
+export default ExpenseItem;
